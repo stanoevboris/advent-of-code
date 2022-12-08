88f9b56222499c57f572cba19d82957daef16dd0
stanoevboris <stanoevboris98@gmail.com>
2022-12-08 23:29:14 +0100

    day 02

--- 2022/02/solution.py
+++ 2022/02/solution.py
@@ -1,16 +1,58 @@
+actions_1 = {'A': 1, 'B': 2, 'C': 3}
+actions_2 = {'X': 1, 'Y': 2, 'Z': 3}
+win = 6
+draw = 3
+lose = 0
+
+
+def game(player_1, player_2):
+    if player_1 == player_2:
+        return draw + player_2
+    elif player_1 == 1 and player_2 == 3:
+        return lose + player_2
+    elif player_1 == 2 and player_2 == 1:
+        return lose + player_2
+    elif player_1 == 3 and player_2 == 2:
+        return lose + player_2
+    elif player_2 == 1 and player_1 == 3:
+        return win + player_2
+    elif player_2 == 2 and player_1 == 1:
+        return win + player_2
+    elif player_2 == 3 and player_1 == 2:
+        return win + player_2
+
+
+def game_2(p1_action, p2_action):
+    if p2_action == 'X':
+        if p1_action == 'A':
+            return lose + actions_2.get('Z')
+        elif p1_action == 'B':
+            return lose + actions_2.get('X')
+        else:
+            return lose + actions_2.get('Y')
+    elif p2_action == 'Y':
+        if p1_action == 'A':
+            return draw + actions_2.get('X')
+        elif p1_action == 'B':
+            return draw + actions_2.get('Y')
+        else:
+            return draw + actions_2.get('Z')
+    else:
+        if p1_action == 'A':
+            return win + actions_2.get('Y')
+        elif p1_action == 'B':
+            return win + actions_2.get('Z')
+        else:
+            return win + actions_2.get('X')
+
 
 with open('input.txt', 'r') as file:
-    snack_groups = file.read().split('\n\n')
+    lines = file.read().split('\n')
 
-group_values = []
-for snack_group in snack_groups:
-    parsed_values = [int(val) for val in snack_group.split('\n')]
-    current_sum = sum(parsed_values)
-    group_values.append(current_sum)
+actions = [line.split() for line in lines]
 
-part_1_result = max(group_values)
-print(part_1_result)
+part_1_score = sum([game(actions_1.get(p1), actions_2.get(p2)) for p1, p2 in actions])
+print(part_1_score)
 
-group_values.sort(reverse=True)
-part_2_result = sum(group_values[:3])
-print(part_2_result)
+part_2_score = sum([game_2(p1, p2) for p1, p2 in actions])
+print(part_2_score)
